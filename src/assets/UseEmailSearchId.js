f6db45f483688c746788becc49bd2b61ae9b341f
jaeseonnamgung <sunnamgung8@naver.com>
2024-08-12 12:01:15 +0900

    08/12 이메일 아이디 찾기 구현

--- src/assets/UseEmailSearchId.js
+++ src/assets/UseEmailSearchId.js
@@ -5,73 +5,254 @@
  *
  * @format
  */
-import React, {useState} from 'react';
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
   TouchableOpacity,
   StyleSheet,
   TextInput,
-} from 'react-native';
+  Alert,
+} from "react-native";
 
-const UseEmailSearchId = ({navigation}) => {
-  const [idInput, setIdInput] = useState('');
+const UseEmailSearchId = () => {
+  const [nameInput, setNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [sendVerificationCodeStatus, setSendVerificationCodeStatus] =
+    useState(false);
+
+  const [errorStatus, setErrorStatus] = useState(false);
+
+  const [emailErrorText, setEmailErrorText] = useState("");
+
+  const [minutes, setMinutes] = useState(3);
+  const [seconds, setSeconds] = useState(0);
+  const navigation = useNavigation();
+  const onChangeEmail = (text) => {
+    setEmailInput(text);
+    const regex =
+      /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
+    if (regex.test(text)) {
+      setEmailErrorText("");
+      return;
+    } else {
+      setEmailErrorText("이메일 형식이 올바르지 않습니다.");
+      return;
+    }
+  };
+
+  const requestVerificationCode = async () => {
+    if (!emailInput || !nameInput || emailErrorText) {
+      setErrorStatus(true);
+      return;
+    }
+
+    const url = "http://localhost:9090/user/login/findId/email";
+    const findUserIdByEmailRequest = {
+      name: nameInput,
+      email: emailInput,
+    };
+    try {
+      const response = await axios.post(url, findUserIdByEmailRequest);
+      console.log(response.data);
+      if (response.data) {
+        Alert.alert("인증 코드", "인증 코드가 전송되었습니다.");
+        setMinutes(3);
+        setSeconds(0);
+        setSendVerificationCodeStatus(true);
+      }
+    } catch (error) {
+      Alert.alert("에러", error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    const countDown = setInterval(() => {
+      setSeconds(seconds);
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      } else {
+        if (minutes === 0) {
+          clearInterval(countDown);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1800);
+    return () => clearInterval(countDown);
+  }, [seconds, minutes]);
+
+  const checkVerificationCode = async () => {
+    if (sendVerificationCodeStatus && !verificationCode) {
+      setErrorStatus(true);
+      return;
+    }
+    const url =
+      "http://localhost:9090/user/login/checkVerificationCode/userId?verificationType=EMAIL";
+    const verificationRequest = {
+      verificationKey: emailInput,
+      verificationCode: verificationCode,
+    };
+
+    try {
+      const response = await axios.post(url, verificationRequest);
+      Alert.alert(
+        "회원 ID",
+        `${nameInput}님의 아이디는 ${response.data.userId} 입니다.`,
+        [
+          {
+            text: "확인"
+          },
+          {
+            text: "로그인",
+            onPress: () => navigation.navigate("Login"),
+          },
+        ]
+      );
+    } catch (error) {
+      Alert.alert("에러", error.response.data.message);
+    }
+  };
 
   return (
     <View>
       <View
         style={{
           gap: 15,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
           paddingTop: 20,
-        }}>
-        <Text style={{fontSize: 16, color: '#000'}}>이름</Text>
+        }}
+      >
+        <Text style={{ fontSize: 16, color: "#000" }}>이름</Text>
         <TextInput
-          style={styles.joinInput}
-          value={idInput}
-          onChange={setIdInput}
+          style={[
+            styles.joinInput,
+            errorStatus &&
+              !nameInput && { borderBottomColor: "red", borderBottomWidth: 1 },
+          ]}
+          autoCapitalize="none"
+          blurOnSubmit={false}
+          returnKeyType="next"
+          onChangeText={setNameInput}
         />
       </View>
       <View
         style={{
           gap: 15,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
           paddingTop: 20,
-        }}>
-        <Text style={{fontSize: 16, color: '#000'}}>이메일</Text>
-        <TextInput
-          style={styles.joinInput}
-          value={idInput}
-          onChange={setIdInput}
-        />
-      </View>
-      <View
-        style={{
-          gap: 15,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          paddingTop: 20,
-        }}>
-        <Text style={{fontSize: 16, color: '#000'}}>인증번호</Text>
+        }}
+      >
+        <Text style={{ fontSize: 16, color: "#000" }}>이메일</Text>
         <View
           style={{
-            flexDirection: 'row',
+            flexDirection: "row",
             gap: 10,
-            width: '80%',
-          }}>
+            width: "80%",
+          }}
+        >
           <TextInput
-            style={styles.joinInputOverlab}
-            value={idInput}
-            onChange={setIdInput}
+            style={[
+              styles.joinInputOverlab,
+              errorStatus &&
+                !emailInput && {
+                  borderBottomColor: "red",
+                  borderBottomWidth: 1,
+                },
+            ]}
+            onChangeText={(text) => onChangeEmail(text)}
+            inputMode="email"
+            autoCapitalize="none"
+            blurOnSubmit={false}
+            returnKeyType="next"
           />
-          <TouchableOpacity style={styles.overlap}>
-            <Text style={{fontSize: 16, fontWeight: 700, color: '#fff'}}>
+          <TouchableOpacity
+            style={styles.overlap}
+            onPress={requestVerificationCode}
+          >
+            <Text style={{ fontSize: 16, fontWeight: 700, color: "#fff" }}>
               인증 요청
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 10,
+        }}
+      >
+        {emailErrorText ? (
+          <Text style={{ color: "red" }}>{emailErrorText}</Text>
+        ) : null}
+      </View>
+
+      <View
+        style={{
+          gap: 15,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingTop: 20,
+        }}
+      >
+        <Text style={{ fontSize: 16, color: "#000" }}>인증번호</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 10,
+            width: "80%",
+          }}
+        >
+          <TextInput
+            style={[
+              styles.joinInputOverlab,
+              errorStatus &&
+                sendVerificationCodeStatus &&
+                !verificationCode && {
+                  borderBottomColor: "red",
+                  borderBottomWidth: 1,
+                },
+            ]}
+            onChangeText={setVerificationCode}
+            autoCapitalize="none"
+            blurOnSubmit={false}
+            returnKeyType="next"
+          />
+          {sendVerificationCodeStatus &&
+          emailInput &&
+          minutes > 0 &&
+          seconds > 0 ? (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: "red" }}>
+                {`${minutes < 10 ? `0${minutes}` : minutes}:${
+                  seconds < 10 ? `0${seconds}` : seconds
+                }`}
+              </Text>
+            </View>
+          ) : null}
+          <TouchableOpacity
+            style={styles.overlap}
+            onPress={checkVerificationCode}
+          >
+            <Text style={{ fontSize: 16, fontWeight: 700, color: "#fff" }}>
+              인증 확인
             </Text>
           </TouchableOpacity>
         </View>
@@ -85,23 +266,24 @@ const styles = StyleSheet.create({
     flex: 3,
     height: 50,
     borderRadius: 30,
-    backgroundColor: '#F7F7F7',
+    backgroundColor: "#F7F7F7",
     paddingLeft: 20,
   },
   joinInput: {
-    width: '80%',
+    direction: "row",
+    width: "80%",
     height: 50,
     borderRadius: 30,
-    backgroundColor: '#F7F7F7',
+    backgroundColor: "#F7F7F7",
     paddingLeft: 20,
   },
   overlap: {
     flex: 2,
     height: 50,
     borderRadius: 30,
-    backgroundColor: '#FEDB37',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#FEDB37",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
